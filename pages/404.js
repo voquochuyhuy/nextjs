3fe30f2dbdc742d0b2603dc4c31328a4b1021ac5
voquochuyhuy <37115874+voquochuyhuy@users.noreply.github.com>
2021-01-08 11:04:05 +0700

    huy commit

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>Oops!...</h1>
+      <img src="/images/nic.png"></img>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
--- pages/example.js
+++ pages/example.js
@@ -1,19 +1,24 @@
 import { useEffect } from "react";
 
 export async function getStaticProps() {
-    const res = await fetch('https://api.github.com/repos/vercel/next.js');
-    const json = await res.json();
-    return {
-      props: {
-        stars : json.stargazers_count
-      }
-    }
-  }
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const json = await res.json();
+  return {
+    props: {
+      stars: json.stargazers_count,
+    },
+  };
+}
 export default function Example(props) {
-    useEffect(() => {
-        return () => {
-            
-        }
-    }, [])
-    return <div style={{textAlign:"center"}}>{props.stars}</div>
+  useEffect(() => {
+    return () => {};
+  }, []);
+  return (
+    <div style={{ textAlign: "center" }}>
+      <strong>Number of start on Github:{`  `}</strong>
+      {props.stars}
+      <h1>Config route on Nextjs is so easy!</h1>
+      <img style={{width:"500px",height:"auto"}} src="/images/thanh.png"></img>
+    </div>
+  );
 }
