1c77196e7cdf8734fd4116b299655d0271f8c787
voquochuyhuy <37115874+voquochuyhuy@users.noreply.github.com>
2021-01-12 10:42:40 +0700

    87978

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ export default function Custom404() {
     <div style={{textAlign:"center"}}>
       <h1>Oops!...</h1>
       <img src="/images/nic.png"></img>
-      <Image src="/images/nic.png"/>
+      <Image src="/images/nic.png" width={264} height={285}/>
       <h1>404 - Page Not Found</h1>
     </div>
   );
